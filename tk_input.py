467e1c8d2c1131cda8f4e2cca4f924bd5c6e5708
echochio <echochio-tw@users.noreply.github.com>
2020-03-29 15:32:53 +0800

    Create tk_input.py

---
+++ tk_input.py
@@ -0,0 +1,22 @@
+import tkinter as tk  
+from functools import partial  
+   
+   
+def call_result(label_result, n1):  
+    result = (n1.get())
+    print (result)
+    # here can call auth .....
+    return
+   
+root = tk.Tk()  
+#root.geometry('400x200+100+200')
+root.title('OTP')  
+input1 = tk.StringVar()  
+  
+labelNum1 = tk.Label(root, text="輸入認證碼 : ").grid(row=1, column=0)  
+labelResult = tk.Label(root)  
+labelResult.grid(row=7, column=2) 
+entryNum1 = tk.Entry(root, textvariable=input1).grid(row=1, column=2)
+call_result = partial(call_result, labelResult, input1)   
+tk.Button(root,width=10, text="確認與退出", command=lambda: [f() for f in [call_result, root.quit]]).grid(row=3, column=0)
+root.mainloop()  
