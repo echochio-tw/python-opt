cba5f13cd47d696bc54e43164911909c0a8689f7
echochio <echochio-tw@users.noreply.github.com>
2020-03-29 15:45:38 +0800

    Update opt_out.py

--- opt_out.py
+++ opt_out.py
@@ -13,7 +13,7 @@ import time
 Secret = 'userechochio' # Secret的不要太長不要超過32
 Secret = base64.b32encode(s=Secret.encode('utf-8'))
 K = base64.b32decode(Secret,True)
-C = struct.pack(">Q", int(time.time()) // 30)   # 60 秒後失效
+C = struct.pack(">Q", int(time.time()) // 30)   # 30 秒後失效
 H = hmac.new(K,C,hashlib.sha1).digest() # 使用hmac sha1加密
 O = H[19] & 15  # bin(15)=00001111=0b1111
 DynamicPasswd = str((struct.unpack(">I", H[O:O+4])[0] & 0x7fffffff) % 1000000)
